473ec3f6c081c273368eae6c5255a4c4bf2dd04a
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-17 21:07:11 +0700

    add transaction validations

--- models/transaction.js
+++ models/transaction.js
@@ -4,13 +4,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let TransactionSchema = new Schema({
-	member: { type: Schema.Types.ObjectId, ref: 'Customer' },
+	member: { 
+		type: Schema.Types.ObjectId, 
+		ref: 'Customer',
+		required: [true, 'Why u no input member?']
+	},
 	days: Number,
 	out_date: Date,
 	due_date: Date,
 	in_date: Date,
 	fine: Number,
-	booklist: [{ type: Schema.Types.ObjectId, ref: 'Book' }]
+	booklist: {
+		type: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
+		required: [true, 'Please input at least one book']
+	}
 });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
