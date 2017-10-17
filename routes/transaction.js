4b5a4e2397168815cfb0e5c5c36b3693ebed0ff2
jasoneffendi <jasoneffendi27@gmail.com>
2017-10-18 02:20:05 +0700

    Update CRUD Transaction, added methodOverride and re-implemented delete

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var methodOverride = require('method-override');
 var router = express.Router();
 var Collection = require('../models/collection')
 var collectionCtrl = require('../controllers/transaction')
 /* GET home page. */
 
+
+router.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+}));
 
 router.get('/', collectionCtrl.get);
 
