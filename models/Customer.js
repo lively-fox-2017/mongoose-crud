8d55434c93d7ef3e2c95dec23567b76bdc057086
Dimitri <deathmitri@gmail.com>
2017-10-17 21:26:41 +0700

    Added dotenv

--- models/Customer.js
+++ models/Customer.js
@@ -1,5 +1,6 @@
+const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/library');
+mongoose.connect(process.env.MONGO_URL);
 
 const Schema = mongoose.Schema;
 
